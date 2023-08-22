191d8a88277d7a913c44d958ee144e6a13e2dba9
cynthia <cynthiakagz@gmail.com>
2023-08-22 22:07:57 +0300

    started

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -1,2 +1,6 @@
 class Toy < ApplicationRecord
+    validates :name, presence: true
+  validates :image, presence: true
+  validates :likes, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
 end
